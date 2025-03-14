100f0ffd35c72c489e57329ac96f86d7129afd09
root <yvesniyigenaa@gmail.com>
2025-03-14 02:58:56 -0700

    sql more queries

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script that lists all privileges of MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Attempt to show grants for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
